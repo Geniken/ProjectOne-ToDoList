28d010f3ceaacec10adbea87aa20c7abd2570b88
Daniel Kim <Msdk5500@gmail.com>
2016-08-25 16:02:46 -0700

    first commit

---
+++ ToDoListProjectOne/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ToDoListProjectOne
+//
+//  Created by Daniel Kim on 8/25/16.
+//  Copyright © 2016 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // project one
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
